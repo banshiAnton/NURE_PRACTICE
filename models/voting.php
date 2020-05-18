b6a0a6cc1f35b5f601e930b5912aafb6eb26576f
banshiAnton <banshi.anton@gmail.com>
2020-05-19 01:13:50 +0300

    add voting model

---
+++ models/voting.php
@@ -0,0 +1,45 @@
+<?php
+
+    class VotingResult {
+        const YES = 1;
+        const NO = 2;
+        const DRAW = 3;
+    }
+
+    class VotingTableFields {
+        const ID = "id";
+        const SUBJECT = "subject";
+        const DESCRIPTION = "description";
+        const CREATED_DATE = "created_date";
+        const OPNED = "opned";
+    }
+
+    class Voting {
+
+        public $id;
+        public $subject;
+        public $description;
+        public $created_date;
+        public $opned;
+
+        public function __construct() {
+
+        }
+
+        public function isOpned() {
+            $this->$opned === 1;
+        }
+
+        public static function createVotingFromSQLRow($sqlRow) {
+            $voting = new Voting();
+
+            $voting->$id = (int)$sqlRow[VotingTableFields::ID];
+            $voting->$subject = $sqlRow[VotingTableFields::SUBJECT];
+            $voting->$description = $sqlRow[VotingTableFields::DESCRIPTION];
+            $voting->$created_date = $sqlRow[VotingTableFields::CREATED_DATE];
+            $voting->$id = (int)$sqlRow[VotingTableFields::OPNED];
+
+            return $voting;
+        }
+    }
+?>
