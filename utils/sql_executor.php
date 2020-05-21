1b90cb5582fda4e84f207c9f404858d09c0bce11
banshiAnton <banshi.anton@gmail.com>
2020-05-21 21:09:14 +0300

    replace sql  preapre by bindParam

--- utils/sql_executor.php
+++ utils/sql_executor.php
@@ -3,8 +3,15 @@
         public static function execute($db_connection, $sqlQuery, $params) {
             $sth = $db_connection->prepare($sqlQuery, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
             if (is_array($params)) {
-                $sth->execute($params);
+                foreach($params as $param_name => $param_value) {
+                    if (is_array($param_value)) {
+                        $sth->bindParam($param_name, $param_value['value'], $param_value['dataType']);
+                    } else {
+                        $sth->bindValue($param_name, $param_value);
+                    }
+                }
             }
+            $sth->execute();
             $result = $sth->fetchAll();
             return $result;
         }
