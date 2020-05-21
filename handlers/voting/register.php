ba747313d2844c9748e7b7513069dbedfe38fc57
banshiAnton <banshi.anton@gmail.com>
2020-05-21 21:08:05 +0300

    clear dumps

--- handlers/voting/register.php
+++ handlers/voting/register.php
@@ -9,7 +9,5 @@
     );
 
     $voting_id = (int)Voting::register($db_connection, $newVoting);
-    var_dump($voting_id);
     $voting = Voting::getById($db_connection, $voting_id);
-    var_dump($voting)
 ?>
