618928ad98d5c7de9dca89cd3cf0c3664f02430d
banshiAnton <banshi.anton@gmail.com>
2020-05-22 21:15:15 +0300

    it can bind params for sql insert

--- utils/sql_executor.php
+++ utils/sql_executor.php
@@ -18,7 +18,16 @@
 
         public static function insert($db_connection, $sqlQuery, $params) {
             $sth = $db_connection->prepare($sqlQuery, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-            $sth->execute($params);
+            if (is_array($params)) {
+                foreach($params as $param_name => $param_value) {
+                    if (is_array($param_value)) {
+                        $sth->bindParam($param_name, $param_value['value'], $param_value['dataType']);
+                    } else {
+                        $sth->bindValue($param_name, $param_value);
+                    }
+                }
+            }
+            $sth->execute();
             return $db_connection->lastInsertId();
         }
     }
