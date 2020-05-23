3b6562d612d124d5c40ec800c738cfd5060d7350
banshiAnton <banshi.anton@gmail.com>
2020-05-23 17:56:27 +0300

    add title to edit votins page

--- views/votings/editing.php
+++ views/votings/editing.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Управление запросами</title>
 </head>
 <body>
     <a href="/nure_practice/views/votings/register.php">Создать новый опрос</a>
