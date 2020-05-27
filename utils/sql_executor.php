fbe6d2ec0d0b11b70d16e24b7215434eb79f3eb9
banshiAnton <banshi.anton@gmail.com>
2020-05-28 00:11:30 +0300

    imp: _sql executor

--- utils/sql_executor.php
+++ utils/sql_executor.php
@@ -1,22 +1,14 @@
 <?php
     class SQLExecutor {
         public static function execute($db_connection, $sqlQuery, $params) {
-            $sth = $db_connection->prepare($sqlQuery, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-            if (is_array($params)) {
-                foreach($params as $param_name => $param_value) {
-                    if (is_array($param_value)) {
-                        $sth->bindParam($param_name, $param_value['value'], $param_value['dataType']);
-                    } else {
-                        $sth->bindValue($param_name, $param_value);
-                    }
-                }
-            }
-            $sth->execute();
-            $result = $sth->fetchAll();
-            return $result;
+            return self::_executor($db_connection, $sqlQuery, $params, false);
         }
 
         public static function insert($db_connection, $sqlQuery, $params) {
+            return self::_executor($db_connection, $sqlQuery, $params, true);
+        }
+
+        private static function _executor($db_connection, $sqlQuery, $params, $is_insert) {
             $sth = $db_connection->prepare($sqlQuery, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
             if (is_array($params)) {
                 foreach($params as $param_name => $param_value) {
@@ -28,7 +20,11 @@
                 }
             }
             $sth->execute();
-            return $db_connection->lastInsertId();
+            if ($is_insert) {
+                return $db_connection->lastInsertId();
+            }
+            $result = $sth->fetchAll();
+            return $result;
         }
     }
 ?>
