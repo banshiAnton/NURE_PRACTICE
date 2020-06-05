50b82818fae87d6fdb4c56c7bffc432c891a66ae
banshiAnton <banshi.anton@gmail.com>
2020-06-05 04:14:42 +0300

    change title

--- views/votings/register.php
+++ views/votings/register.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SignUp</title>
+    <title>Создание опроса</title>
 </head>
 <body>
     <form action="/nure_practice/handlers/voting/register.php" method="post">
